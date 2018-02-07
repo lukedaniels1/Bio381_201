ec97b375674e0ca01f298bcd90049c2a845a8bc2
ldaniels <ldaniels@github.com>
2018-02-07 15:36:38 -0500

    Update

--- FirstProject/R Script Lessons/R Script Lessons .R
+++ FirstProject/R Script Lessons/R Script Lessons .R
@@ -4,6 +4,10 @@
 
 library(ggplot2)
 
+install.packages("devtools")
+library(devtools)
+install_github("thomasp85/patchwork") 
+
 # Using the assigment operator 
 x <- 5 #preferred 
 print(x) 
@@ -212,3 +216,138 @@ head(myVector)
 
 
 #---------------------------------------------End Feb. 1st 2018 
+
+# Using rep to repeat elements and create vectors 
+
+rep(0.5, 6) 
+rep("mystring", 3) 
+rep(x=0.5, times=6)
+rep(times=6, x=0.5)
+myVec <- c(1,2,3) 
+rep(myVec, times=2)
+rep(x=myVec,each = 2) 
+rep(x=myVec, times=myVec)
+rep(x=1:3,times=3:1)
+
+# Seq for creating sequences
+
+seq(from=2, to=4) 
+
+  #Can be used to create non-integer values 
+seq(from=2, to=4,by=0.5)
+seq(from=2, to=4, length=7) #length is the number of elements
+
+x <- seq(from=2, to=4, length=7)
+1:length(x)  # Need to make distinction between contents and how long the vector is
+seq_along(x) #faster, better
+seq_len(10) #Here the input is a certain number
+
+# Why do we want to use sequence along function? Suppose
+
+x <- vector(mode="numeric", length = 0)
+str(x)
+1:length(x) #Produces bad sequence
+
+#To prevent a loop and strange errors later use "Seq_Along"
+seq_along(x) 
+
+# using random numbers 
+
+runif(1)
+
+# To make the same random number generate every time 
+set.seed(100) 
+runif(1)
+runif(n=5, min=100, max= 200)
+
+library(ggplot2) #graphics library 
+z <- runif(n=1000,min=30,max=300)
+qplot(x=z)
+
+#The other random number generator  - Random Normal Distribution 
+
+z <- rnorm(n=1000)
+qplot(x=z)
+
+z <- rnorm(n=1000,mean=30, sd=20)
+qplot(x=z) 
+
+# Use sample function to draw from an existing vector 
+
+longVec <- seq_len(10)
+longVec 
+sample(x=longVec)
+sample(x=longVec, size=3) #Sample Without Replacement 
+
+sample(x=longVec, size=3, replace=TRUE)
+
+# We can randomly sample from atomoic vector with different weights
+myWeights <- c(rep(20,5), rep(100, 5))  # Creates a vector with 20 repeated 5 times and the number 100 repeated 5 times
+myWeights
+
+sample(x=longVec, replace=TRUE, prob = myWeights) 
+
+#subsetting of atomic vectors 
+
+z <- c(3.1, 9.2, 1.3, 0.4, 7.5) 
+
+# 4 ways of subsetting on positive index values 
+
+z[2]
+z[c(2,3)]
+
+# subset on negative index values 
+z[-c(2,3)] #negative in front specifies what values to hold out 
+
+# Can also subset data by creating a boolean vector to select elements that meet a condition 
+
+z<3  
+z[z<3]  #We are inserting a vector of TRUE and FALSE's.  We get only the elements that correspond
+#to those locations where it was specified 
+
+which(z<3) #In the 3rd and 4th locations in the vector, these are the ones that meet criteria 
+
+myCriteria <- z<3
+z[myCriteria]
+z[which(z<3)] #The which function will generate those particular index values
+
+zx <- c(NA,z) # Creating new variable and we put together a missing value with a missing value in the first place
+zx[zx<3] #missing values dropped 
+
+# If we do it this way...
+zx[which(zx<3)] # The missing value (NA) is dropped
+
+## The above function is a great way to drop missing values in a data set 
+
+# keep entire vector
+z[]
+z[-(length(z):(length(z)-2))]  # Translation "Get rid of 5,4 and 3 and give me whats left
+
+# Subset on names of vector elements 
+z
+names(z) <- letters[seq_along(z)] # Assesing lowercase z to the names of these elements 
+print(z) 
+
+z[c("b","d","e")]
+
+# Arithmetic Operators 
+10+3
+10-3
+10 * 3
+10 / 3
+10^3
+log(10) #base 2
+log10(10) #base 10
+
+# modulus operator (remainder of division) 
+10 %% 3  #The remainder of 10 / 3 is 1 
+
+#integer division 
+
+10 %/% 3
+
+# generate the set of all numbers from 1 to 100 that are divisible by 9 
+
+q <- seq_len(100)  # We want code that subsets these numbers by 9 
+
+q[q%%9 == 0] # Takes each element and divides by 9 and asks if remainder is 0
